98d741e544b31bfdc59d64cee400edf8930d556a
Paul Crescini <pcrescini@gmail.com>
2023-09-11 16:26:32 -0500

    update: Added list of blog posts to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
+import { getBlogPostList } from '@/helpers/file-helpers';
 
 import styles from './homepage.module.css';
 
+const BLOG_POSTS = await getBlogPostList();
+
 function Home() {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <h1 className={styles.mainHeading}>Latest Content:</h1>
-
-        {/* TODO: Iterate over the data read from the file system! */}
-        <BlogSummaryCard
-          slug="example"
-          title="Hello world!"
-          abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-          publishedOn={new Date()}
-        />
-      </div>
-    </>
+    <div className={styles.wrapper}>
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
+      {BLOG_POSTS.map(({ slug, title, abstract, publishedOn }) => {
+        return (
+          <BlogSummaryCard
+            key={slug}
+            slug={slug}
+            title={title}
+            abstract={abstract}
+            publishedOn={publishedOn}
+          />
+        );
+      })}
+    </div>
   );
 }
 
