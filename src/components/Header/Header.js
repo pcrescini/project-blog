83737d01e8ba5f8f4a936c68d7a61801f0a49977
Paul Crescini <pcrescini@gmail.com>
2023-09-13 17:54:36 -0500

    update: Started light / dark mode implementation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,7 @@ import styles from './Header.module.css';
 
 function Header({ theme, className, ...delegated }) {
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -24,15 +21,11 @@ function Header({ theme, className, ...delegated }) {
               transform: 'translate(2px, -2px)',
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
         <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+          {theme === 'light' ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
