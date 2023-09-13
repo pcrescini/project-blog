aa3e9d416a9a91aa29d8d7b93e14c433c74eb39e
Paul Crescini <pcrescini@gmail.com>
2023-09-12 20:08:35 -0500

    update: Added Framer Motion to includeRemainderArea

--- src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -22,6 +22,8 @@ function DivisionGroupsDemo({
 
   const remainder = includeRemainderArea ? numOfItems % numOfGroups : null;
 
+  const id = React.useId();
+
   // When we're splitting into 1-3 groups, display side-by-side
   // columns. When we get to 4, it should switch to a 2x2 grid.
   const gridStructure =
@@ -36,60 +38,69 @@ function DivisionGroupsDemo({
 
   return (
     <MotionConfig reducedMotion="user">
-      <LayoutGroup>
-        <Card as="section" className={styles.wrapper}>
-          <header className={styles.header}>
-            <SliderControl
-              label="Number of Groups"
-              className={styles.slider}
-              step={1}
-              min={1}
-              max={4}
-              value={numOfGroups}
-              onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
-            />
-          </header>
-
-          <div className={styles.demoWrapper}>
-            <div className={clsx(styles.demoArea)} style={gridStructure}>
-              {range(numOfGroups).map((groupIndex) => (
-                <div key={groupIndex} className={styles.group}>
-                  {range(numOfItemsPerGroup).map((index) => {
-                    return (
-                      <motion.div
-                        layout="position"
-                        key={index}
-                        className={styles.item}
-                        transition={{
-                          type: 'spring',
-                          stiffness: 400,
-                          damping: 40 + index * 5,
-                        }}
-                      />
-                    );
-                  })}
-                </div>
-              ))}
-            </div>
-          </div>
-
-          {includeRemainderArea && (
-            <div className={styles.remainderArea}>
-              <p className={styles.remainderHeading}>Remainder Area</p>
-
-              {range(remainder).map((index) => {
-                return <div key={index} className={styles.item} />;
-              })}
-            </div>
-          )}
-
-          <Equation
-            dividend={numOfItems}
-            divisor={numOfGroups}
-            remainder={remainder}
+      <Card as="section" className={styles.wrapper}>
+        <header className={styles.header}>
+          <SliderControl
+            label="Number of Groups"
+            className={styles.slider}
+            step={1}
+            min={1}
+            max={4}
+            value={numOfGroups}
+            onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
           />
-        </Card>
-      </LayoutGroup>
+        </header>
+
+        <div className={styles.demoWrapper}>
+          <div className={clsx(styles.demoArea)} style={gridStructure}>
+            {range(numOfGroups).map((groupIndex) => (
+              <div key={groupIndex} className={styles.group}>
+                {range(numOfItemsPerGroup).map((index) => {
+                  return (
+                    <motion.div
+                      layout={true}
+                      key={index}
+                      className={styles.item}
+                      transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 40 + index * 5,
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {includeRemainderArea && (
+          <div className={styles.remainderArea}>
+            <p className={styles.remainderHeading}>Remainder Area</p>
+
+            {range(remainder).map((index) => {
+              return (
+                <motion.div
+                  layout={true}
+                  key={index}
+                  className={styles.item}
+                  transition={{
+                    type: 'spring',
+                    stiffness: 400,
+                    damping: 40 + index * 5,
+                  }}
+                />
+              );
+            })}
+          </div>
+        )}
+
+        <Equation
+          dividend={numOfItems}
+          divisor={numOfGroups}
+          remainder={remainder}
+        />
+      </Card>
     </MotionConfig>
   );
 }
