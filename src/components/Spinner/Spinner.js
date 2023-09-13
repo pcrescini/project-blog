1e63b98f875b9785857de332bf338feba307a887
Paul Crescini <pcrescini@gmail.com>
2023-09-12 21:34:47 -0500

    update: Added Spinner component

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Loader } from 'react-feather';
 
-function Spinner() {
-  return <div></div>;
-}
+import styles from './Spinner.module.css';
+
+const Spinner = ({ color, size }) => {
+  return (
+    <div className={styles.wrapper}>
+      <Loader color={color} size={size} />
+    </div>
+  );
+};
 
 export default Spinner;
